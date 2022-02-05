82b38b6a05304875b5d4e8c81425baa2a62ede8b
Christopher Mossman <christopher.mossman@ucalgary.ca>
2022-02-04 19:05:00 -0700

    Passing np array to external Python code works.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,12 @@
 #include <tuple>
 #include <pybind11/embed.h>
 #include <pybind11/stl.h> // For "stl" conversions (list, set, dict, etc.)
+#include <pybind11/numpy.h>
+
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
 namespace py = pybind11;
 using namespace pybind11::literals; // to bring in the `_a` literal
 
@@ -19,39 +25,72 @@ int main() {
     py::print("Env Path: ", os.attr("environ").attr("get")("PATH"));
     py::print(sys.attr("path"), "flush"_a=true);
 
-
-
-    std::cout << "Before import test." << std::endl;
     py::object scope = py::module_::import("__main__").attr("__dict__");
     py::exec(
         "d = {'zero': 0, 'two': 2, 'three': 3}\n"
         "print(d)",
         scope);
-    py::print("Now testing pandas...", "flush"_a=true);
-    auto pd = py::module::import("pandas");
-    py::print(pd.attr("__version__"), "flush"_a = true);
+
+    cv::VideoCapture cap(0);
+    // Check if camera opened successfully
+    if (!cap.isOpened()) {
+            std::cout << "Error starting the webcam capture!" << std::endl;
+            return -1;
+    }
 
     py::print("Now testing my own local .py file...", "flush"_a=true);
+    // Since I'm working with a different folder, my "hack" solution is to
+    // just add said folder to sys.path
+    sys.attr("path").attr("insert")(1, "/home/u1/Documents/FunCoding/learningPybind");
     // Performs "from example import myPyClass
-    py::object myPyClass = py::module_::import("customPython.example").attr("myPyClass");
-    py::object mpc = myPyClass(1, 1);
-    py::object len1 = mpc.attr("euclength")();
-    float floatLen1 = len1.cast<float>();
-    std::cout << "length of origin to mpc before translation: " << floatLen1 << std::endl;
-    mpc.attr("translate")(2, 3);
-    py::object len2 = mpc.attr("euclength")();
-    float floatLen2 = len2.cast<float>();
-    std::cout << "length of origin to mpc after translation: " << floatLen2 << std::endl;
-    mpc.attr("translate")(0.1, 0.2);
-    py::list tripleList = mpc.attr("arrayOfTriples")();
-    std::vector<py::object> tripleVector = tripleList.cast<std::vector<py::object>>();
-    py::tuple thirdTriplePy = tripleList[3];
-    auto element0 = thirdTriplePy[0];
-    py::print("[3][0]", element0, "flush"_a = true);
-    float finalTupleThing = element0.cast<float>();
+    py::object getRunner = py::module_::import("HandMesh.runnerCreator").attr("getRunner");
+    py::object testNumpyInput = py::module_::import("HandMesh.runnerCreator").attr("testNumpyInput");
+    py::object runner = getRunner(634, 640);
 
-    float sqr = finalTupleThing * finalTupleThing;
-    std::cout << "sqr: " << sqr << std::endl;
+    py::print("kMatrix:", runner.attr("kMatrix"), "flush"_a=true);
+
+
+    cv::Mat frame;
+    while (true) {
+        cap >> frame;
+
+        cv::Mat frameBGR;
+        cv::cvtColor(frame, frameBGR, cv::COLOR_BGR2RGB);
+        py::array myArray(frameBGR.total() * frameBGR.elemSize(), frameBGR.data);
+        py::array reshaped = myArray.reshape({480, 640, 3});
+        py::object result = runner.attr("processFrame")(reshaped);
+
+//        py::array points2D = result.attr("")
+
+        cv::imshow("result", frame);
+
+        int key = cv::waitKey(1);
+        if(key == (int)'q')
+            break;
+    }
+
+
+
+
+
+
+//    py::object len1 = mpc.attr("euclength")();
+//    float floatLen1 = len1.cast<float>();
+//    std::cout << "length of origin to mpc before translation: " << floatLen1 << std::endl;
+//    mpc.attr("translate")(2, 3);
+//    py::object len2 = mpc.attr("euclength")();
+//    float floatLen2 = len2.cast<float>();
+//    std::cout << "length of origin to mpc after translation: " << floatLen2 << std::endl;
+//    mpc.attr("translate")(0.1, 0.2);
+//    py::list tripleList = mpc.attr("arrayOfTriples")();
+//    std::vector<py::object> tripleVector = tripleList.cast<std::vector<py::object>>();
+//    py::tuple thirdTriplePy = tripleList[3];
+//    auto element0 = thirdTriplePy[0];
+//    py::print("[3][0]", element0, "flush"_a = true);
+//    float finalTupleThing = element0.cast<float>();
+
+//    float sqr = finalTupleThing * finalTupleThing;
+//    std::cout << "sqr: " << sqr << std::endl;
 
     std::cout << "End of tests" << std::endl;
     return EXIT_SUCCESS;
